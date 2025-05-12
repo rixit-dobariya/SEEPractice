8da5b4018d983b7398be4a5e6713ccb76cf75ef1
Rixit Dobariya <rixitdobariya05@gmail.com>
2025-05-12 21:30:52 +0530

    insert in  asp.net web application

---
+++ WebApplication2/AddStudent.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddStudent.aspx.cs" Inherits="WebApplication2.AddStudent" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Name: <asp:TextBox ID="txtName" runat="server" Height="22px" Width="128px"></asp:TextBox>
+            <br />
+            Roll no:<asp:TextBox ID="txtRollNo" runat="server"></asp:TextBox>
+            <br />
+            Course:<asp:TextBox ID="txtCourse" runat="server"></asp:TextBox>
+            <br />
+            Age:<asp:TextBox ID="txtAge" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Add Student" OnClick="Button1_Click" />
+            <br />
+            <asp:Label ID="Label1" runat="server" ForeColor="Green"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/AddStudent.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class AddStudent : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\01\\source\\repos\\SEEPractice\\SEEPractice\\App_Data\\Student.mdf;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("insert into student2 values(@rollno, @name, @course, @age)",con);
+            cmd.Parameters.AddWithValue("name",txtName.Text);
+            cmd.Parameters.AddWithValue("rollno", txtRollNo.Text);
+            cmd.Parameters.AddWithValue("course", txtCourse.Text);
+            cmd.Parameters.AddWithValue("age", txtAge.Text);
+            con.Open();
+            if (cmd.ExecuteNonQuery() >= 1)
+            {
+                txtName.Text = "";
+                txtRollNo.Text = "";
+                txtCourse.Text = "";
+                txtAge.Text = "";
+                Label1.Text = "Student added successfully!";
+            }
+            con.Close();
+        }
+    }
+}
